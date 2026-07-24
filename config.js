913ee60b687215f8099ea0c5e5ecf4b3779f4472
PPOJamie <j.kirkpatrick@packagingproductsonline.co.uk>
2026-07-24 17:30:03 +0100

    Update config.js
    Connect HideLine to Supabase

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
  * You can also leave these blank and configure Connected Mode inside the app.
  */
 window.HIDELINE_CONFIG = {
-  supabaseUrl: "",
-  supabaseAnonKey: "",
+  supabaseUrl: "https://keoijlhwtqlztdsxixxn.supabase.co/",
+  supabaseAnonKey: "sb_publishable_Y7fpSw8QnhwPIbBZ1Yo8cw_k-kkT-_N",
   googleMapId: "1lDtKjR7rN1zelD3FjepU1XNvHmnb774"
 };
